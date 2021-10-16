45770b327c97990da7a4f8f58a19b529b668be7c
Khaled-Walid <justkhwf@outlook.com>
2021-10-16 08:53:22 +0200

    - add StoredUsers component - add module css style to StoredUsers component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import AddNewUser from "./components/Users/AddNewUser";
+import StoredUsers from "./components/Users/StoredUsers";
 
 function App() {
   return (
     <div>
       <AddNewUser></AddNewUser>
+      <StoredUsers users={[]}></StoredUsers>
     </div>
   );
 }
---
+++ src/components/Users/StoredUsers.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import Card from "../UI/Card";
+import classes from "./StoredUsers.module.css";
+
+function StoredUsers(props) {
+  const usersList = props.users.map((entry) => {
+    return (
+      <li>
+        {entry.name}, {entry.age} years old
+      </li>
+    );
+  });
+  return (
+    <Card className={classes.users}>
+      <ul>{usersList}</ul>;
+    </Card>
+  );
+}
+export default StoredUsers;
